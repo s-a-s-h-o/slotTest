13398aee93c16432fa76d887f497a6379fdf7b11
Leskitoo12345 <aleksandyr.v.aleksiev@gmail.com>
2019-06-16 00:02:17 +0300

    add some vuetify comp and rename existing coms

---
+++ components/headerTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">this is header</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+  color: yellow;
+  padding: 20px;
+}
+</style>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,24 +1,130 @@
 <template>
-	<testOne>
-      <div slot="head"><testTwo/></div>
-      <div slot="messageBox"><testThree/></div>
-	</testOne>
+  <appWrapper>
+    <div slot="head">
+      <headerTest/>
+    </div>
+    <div slot="messageBox">
+      <!-- <testThree/> -->
+      <div>
+        <div class="text-xs-center">
+          <v-btn color="primary" @click="alert = !alert">Toggle</v-btn>
+        </div>
+        <v-alert
+          :value="alert"
+          type="success"
+          transition="scale-transition"
+        >This is a success alert.</v-alert>
+      </div>
+    </div>
+    <div slot="body">
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>{{ card_text }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>{{ card_text }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>{{ card_text }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>{{ card_text }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </div>
+    <div class="footerWrapper" slot="footer">
+      <v-footer class="pa-3">
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+      </v-footer>
+    </div>
+  </appWrapper>
 </template>
 
 <script>
-import testOne from "../components/testOne"
-import testTwo from "../components/testTwo"
-import testThree from "../components/testThree"
+import appWrapper from "../components/appWrapper";
+import headerTest from "../components/headerTest";
+import testThree from "../components/testThree";
 
 export default {
-  components:{
-    testOne,
-    testTwo,
+  components: {
+    appWrapper,
+    headerTest,
     testThree
+  },
+  data() {
+    return {
+      alert: true,
+      card_text:"custom text"
+    };
+  },
+  mounted() {
+    console.log("this.$vuetify.theme", this.$vuetify.theme);
   }
-}
+};
 </script>
 
 <style>
-
+.flex {
+  padding: 10px;
+}
+.footerWrapper .v-footer {
+  background: green;
+  padding: 20px !important;
+  height: 100px !important;
+}
 </style>
